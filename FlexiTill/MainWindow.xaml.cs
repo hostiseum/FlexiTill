65f430b020779b01c2efc139c38d030ef97085ef
Kalyana Chakravarthi Medavarapu <kalyanan_m@hotmail.com>
2016-04-17 17:15:04 +0100

    Added ArticleKeys and Bread Services

--- FlexiTill/MainWindow.xaml.cs
+++ FlexiTill/MainWindow.xaml.cs
@@ -20,9 +20,200 @@ namespace FlexiTill
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string[] pageNames = { "NumbersPage", "ArticleKeysPage", "BakeryServicesPageOne", "BakeryServicesPageTwo", "BreadRollPage", "WrapsPage", "TeaCakesPage", "CakesPage", "TraditionalToastingPage", "LotteryPage", "FruitVegPage", "EntertainmentPage", "DepartmentsPage", "CardsPage" };
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void btnArticleKeys_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPage("ArticleKeysPage");
+            
+            Navigation.Text += "ArticleKeys";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            ShowPage("NumbersPage");
+        }
+
+        private void BakeryServices_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPage("BakeryServicesPageOne");
+
+            Navigation.Text += ">> Bakery Services";
+        }
+
+        private void Bread_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPage("BakeryServicesPageTwo");
+
+            Navigation.Text += ">> Bread";
+        }
+
+        private void BreadRoll_Click(object sender, RoutedEventArgs e)
+        {
+
+            ShowPage("BreadRollPage");
+            BreadRollPage.Visibility = Visibility.Visible;
+
+            Navigation.Text += ">> BreadRoll";
+        }
+
+        private void WPN_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPage("WrapsPage");
+            WrapsPage.Visibility = Visibility.Visible;
+
+            Navigation.Text += ">> Wraps, Pitta and Naan";
+        }
+
+        private void CBP_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPage("CrossiantsPage");
+            CrossiantsPage.Visibility = Visibility.Visible;
+
+            Navigation.Text += ">> Crossiants Brioche, Pastries ";
+        }
+
+        private void TToast_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPage("TraditionalToastingPage");
+
+            Navigation.Text += ">> Traditional Toasting ";
+        }
+
+        private void CakesPies_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPage("CakesPage");
+
+            Navigation.Text += ">> Cake and Pies ";
+        }
+
+        //private void FreshBakery_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ArticleKeys.Visibility = Visibility.Collapsed;
+        //    Numbers.Visibility = Visibility.Collapsed;
+        //    BakeryServicesPageOne.Visibility = Visibility.Collapsed;
+        //    BakeryServicesPageTwo.Visibility = Visibility.Collapsed;
+        //    FreshBakeryP.Visibility = Visibility.Visible;
+
+        //    Navigation.Text += ">> Traditional Toasting ";
+        //}
+
+        private void SpecialistBakery_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPage("SpecialistBakery");
+
+            Navigation.Text += ">> Traditional Toasting ";
+        }
+
+        private void TFS_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPage("TeaCakesPage");
+
+            Navigation.Text += ">> Tea Cakes";
+        }
+
+        private void Wraps_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPage("WrapsPage");
+
+            Navigation.Text += ">> xx Toasting ";
+
+        }
+
+        private void Pitta_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Naan_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Crossiants_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BakeryS_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CrMuPa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TCFLS_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CelebrationCake_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SmallCakes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OrganicCake_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CakeSlices_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SeasonalCakes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FruitPie_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HealthierCake_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FlapJack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SweetTreats_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+      
+        private void ShowPage(string pageName)
+        {
+
+            foreach(string page in pageNames)
+            {
+                if (page != pageName)
+                    (this.FindName(page) as StackPanel).Visibility = Visibility.Collapsed;
+            }
+
+            //ArticleKeysPage.Visibility = Visibility.Visible;
+             (this.FindName(pageName) as StackPanel).Visibility = Visibility.Visible;
+        }
+
     }
 }
---
+++ TestWPF/Command.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestWPF
+{
+    public class Command : ICommand
+    {
+        public Action Action { get; set; }
+
+        public void Execute(object parameter)
+        {
+            if (Action != null)
+                Action();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return IsEnabled;
+        }
+
+        private bool _isEnabled;
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                _isEnabled = value;
+                if (CanExecuteChanged != null)
+                    CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action action)
+        {
+            Action = action;
+        }
+    }
+}
